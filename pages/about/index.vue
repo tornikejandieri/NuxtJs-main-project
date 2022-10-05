19ba4de542617928fd0b9f2c5595f6042cbd1484
Tornike Jandieri <108745129+tornikejandieri@users.noreply.github.com>
2022-10-05 16:09:25 +0400

    file setup and global font

---
+++ pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about-page">
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
+      accusantium libero molestias ipsam? Totam tempora non architecto quisquam
+      suscipit perferendis vel voluptatem assumenda, minus iste ea dolor
+      perspiciatis repellat unde!
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
