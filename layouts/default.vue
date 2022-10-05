51694ab055420ce77e91984cbd6fd6a8a1aed9ab
Tornike Jandieri <108745129+tornikejandieri@users.noreply.github.com>
2022-10-05 16:54:24 +0400

    header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav" />
+    <TheSidenav :show="displaySidenav" @close="displaySidenav = false" />
     <nuxt />
   </div>
 </template>
 
 <script>
-export default {};
+import TheHeader from "~/components/Navigation/TheHeader.vue";
+import TheSidenav from "~/components/Navigation/TheSidenav.vue";
+export default {
+  components: { TheHeader, TheSidenav },
+  data() {
+    return {
+      displaySidenav: false,
+    };
+  },
+};
 </script>
 
 <style>
