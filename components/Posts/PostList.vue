23ec85e1609c6851b0668e7f76eff1bb41fe50fe
Tornike Jandieri <108745129+tornikejandieri@users.noreply.github.com>
2022-10-05 18:25:23 +0400

    admin

--- components/Posts/PostList.vue
+++ components/Posts/PostList.vue
@@ -2,18 +2,21 @@
   <section class="post-list">
     <PostPreview
       id="1"
+      :is-admin="isAdmin"
       thumbnail="https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?w=2000"
       title="Hello There!"
       previewText="This is my first post!"
     />
     <PostPreview
       id="2"
+      :is-admin="isAdmin"
       thumbnail="https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?w=2000"
       title="Hello There! for the second time"
       previewText="This is my second post!"
     />
     <PostPreview
       id="3"
+      :is-admin="isAdmin"
       thumbnail="https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?w=2000"
       title="Hi! for the third time "
       previewText="This is my third post!"
@@ -25,6 +28,12 @@
 import PostPreview from "~/components/Posts/PostPreview.vue";
 export default {
   components: { PostPreview },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
 };
 </script>
 
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from "~/components/Admin/AdminPostForm.vue";
+export default {
+  components: { AdminPostForm },
+  data() {
+    return {
+      loadedPost: {
+        author: "Tornike",
+        title: "My awesome Post",
+        content: "Super Amazing, thanks for that!",
+        thumbnailLink:
+          "https://img.freepik.com/free-vector/laptop-with-program-code-isometric-icon-software-development-programming-applications-dark-neon_39422-971.jpg?w=2000",
+      },
+    };
+  },
+  layout: "admin",
+};
+</script>
