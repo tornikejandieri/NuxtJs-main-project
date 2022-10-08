1f7e72e926d45f7d9c4641d89c8dcc233e395f06
Tornike Jandieri <108745129+tornikejandieri@users.noreply.github.com>
2022-10-08 18:59:53 +0400

    axios with api token

--- pages/admin/auth/index.vue
+++ pages/admin/auth/index.vue
@@ -1,15 +1,21 @@
 <template>
   <div class="admin-auth-page">
     <div class="auth-container">
-      <form>
-        <AppControlInput type="email">E-Mail Address</AppControlInput>
-        <AppControlInput type="password">Password</AppControlInput>
-        <AppButton type="submit">{{ isLogin ? 'Login' : 'Sign Up' }}</AppButton>
+      <form @submit.prevent="onSubmit">
+        <AppControlInput type="email" v-model="email"
+          >E-Mail Address</AppControlInput
+        >
+        <AppControlInput type="password" v-model="password"
+          >Password</AppControlInput
+        >
+        <AppButton type="submit">{{ isLogin ? "Login" : "Sign Up" }}</AppButton>
         <AppButton
           type="button"
           btn-style="inverted"
           style="margin-left: 10px"
-          @click="isLogin = !isLogin">Switch to {{ isLogin ? 'Signup' : 'Login' }}</AppButton>
+          @click="isLogin = !isLogin"
+          >Switch to {{ isLogin ? "Signup" : "Login" }}</AppButton
+        >
       </form>
     </div>
   </div>
@@ -28,7 +34,21 @@ export default {
   },
   data() {
     return {
-      isLogin: true
+      isLogin: true,
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    onSubmit(){
+      this.$axios.$post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=[API_KEY]', {
+        email:,
+        password:,
+        returnSecureToken: true
+      }).then(result => {console.log(result)})
+      .catch(e => {console.log(e)})
+
+
     }
   }
 }
@@ -49,4 +69,3 @@ export default {
   box-sizing: border-box;
 }
 </style>
-
